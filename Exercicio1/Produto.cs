58fde43f84f5d3e944f514d47ca145720a399b12
Gabriel Leite Lopes <gabriel.llopes@totvs.com.br>
2024-07-03 14:05:11 -0300

    exercicio 1

---
+++ Exercicio1/Produto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public class Produto
+    {
+        private int _estoque;
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get { return _estoque; } }
+
+        public Produto(string nome, decimal preco, int estoque)
+        {
+            Preco = preco;
+            Nome = nome;
+            _estoque = estoque;
+        }
+
+        public void AdicionarItemAoEstoque(int quantidadeParaAdicionar)
+        {
+            ValidarQuaatidadeDeItens(quantidadeParaAdicionar);
+
+            _estoque += quantidadeParaAdicionar;
+        }
+
+        public void RemoverItensDoEstoque(int quantidadeParaRemover)
+        {
+            ValidarQuaatidadeDeItens(quantidadeParaRemover);
+
+            _estoque -= quantidadeParaRemover;
+
+        }
+
+        private void ValidarQuaatidadeDeItens(int quantidade)
+        {
+            if (quantidade <= 0)
+            {
+                throw new ArgumentException("Quantidade Invalida " + quantidade);
+            }
+        }
+    }
+}
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercicio1;
+
+Console.WriteLine("Hello, World!");
+
+
+var KodakPortra = new Produto("Kodak Portra", 109, 10);
+
+KodakPortra.AdicionarItemAoEstoque(2);
+
+Console.WriteLine($"{KodakPortra.Nome} - Quantidade em estoque: {KodakPortra.Estoque}");
+
+KodakPortra.RemoverItensDoEstoque(1);
+
+Console.WriteLine($"{KodakPortra.Nome} - Quantidade em estoque: {KodakPortra.Estoque}");
+
+
+try
+{
+    KodakPortra.AdicionarItemAoEstoque(-1);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
