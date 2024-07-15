05c60a824b8cf90634b96bfff42f914a93a48b1e
Gabriel Leite Lopes <gabriel.llopes@totvs.com.br>
2024-07-15 13:56:14 -0300

    exercicio macoratti

---
+++ cursoFoop_Exercicio_SOLID1/cursoFoop_Exercicio_SOLID1/ExportaClientesCsv.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cursoFoop_Exercicio_SOLID1
+{
+    public class ExportaClientesCsv: IExportaClientes
+    {
+
+        public ExportaClientesCsv()
+        {
+        }
+
+        public string ExportaClientes(List<Cliente> clientes)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in clientes)
+            {
+                sb.AppendFormat($"{item.Nome},{item.Pais},{item.Email}");
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ cursoFoop_Exercicio_SOLID1/cursoFoop_Exercicio_SOLID1/IExportaClientes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cursoFoop_Exercicio_SOLID1
+{
+    public interface IExportaClientes
+    {
+        string ExportaClientes(List<Cliente> clientes);
+    }
+}
